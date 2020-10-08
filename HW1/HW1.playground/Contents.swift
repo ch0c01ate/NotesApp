8d8b39de7681b79fdf8420f23d6b727e1e1f548f
Viacheslav Bernadzikovskyi <vbernadzikovskiy@gmail.com>
2020-10-08 23:22:17 +0300

    HW1 finished

---
+++ HW1/HW1.playground/Contents.swift
@@ -0,0 +1,107 @@
+import UIKit
+
+struct Note: Hashable {
+    var name: String
+    var text: String
+    var tags: Set<String>
+    var isFavourite = false
+    var creationDate = Date()
+    var deletionDate: Date?
+    
+    private(set) var id: Int
+    
+    private static var idFactory = 0
+    
+    private static func getUniqueIdentifier() -> Int {
+        idFactory += 1
+        return Note.idFactory
+    }
+
+    init(named name:String, text: String = "", tags: Set<String> = []) {
+        self.id = Note.getUniqueIdentifier()
+        self.name = name
+        self.text = text
+        self.tags = tags
+    }
+    
+    // Could be useful, maybe delete later
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+    
+    static func == (lhs: Self, rhs: Self) -> Bool {
+        return lhs.id == rhs.id
+    }
+}
+
+class NoteDataManager {
+    private(set) var notes = [Note]()
+    private var deletedNotes = [Note]()
+    
+    func createNote(named name:String, text: String = "", tags: Set<String> = []) {
+        if(isNoteNameUnique(name)){
+            notes.append(Note(named: name, text: text, tags: tags))
+        } else {
+            // TODO: throw error or maybe reimplement to return true/false
+        }
+    }
+    
+    func getNote(with id:Int) -> Note? {
+        return notes.filter({ $0.id == id }).first
+    }
+    
+    func updateNote(with id: Int, name: String, text: String, tags: Set<String>) {
+        if let index = notes.firstIndex(where: {$0.id == id}) {
+            notes[index].name = name
+            notes[index].text = text
+            notes[index].tags = tags
+        }
+    }
+    
+    func deleteNote(with id: Int) {
+        if let index = notes.firstIndex(where: {$0.id == id}) {
+            deletedNotes.append(notes[index])
+            notes.remove(at: index)
+        }
+    }
+    
+    func restoreNote(with id: Int) {
+        if let index = deletedNotes.firstIndex(where: {$0.id == id}) {
+            notes.append(notes[index])
+            deletedNotes.remove(at: index)
+        }
+    }
+    
+    func switchFavourite(forNoteWithId id: Int) {
+        if let index = notes.firstIndex(where: {$0.id == id}) {
+            notes[index].isFavourite = !notes[index].isFavourite
+        }
+    }
+    
+    // TODO: add creation date if neccessary
+    func filterBy(tags: Set<String>) -> Array<Note> {
+        return notes.filter {tags.isSubset(of: $0.tags)}
+    }
+    
+    func searchBy(name: String) -> Array<Note> {
+        return notes.filter {$0.name.localizedLowercase.contains(name)}
+    }
+    
+    func sortedByDate() -> Array<Note> {
+        return notes.sorted(by: {
+            $0.creationDate.compare($1.creationDate) == .orderedDescending
+        })
+    }
+    
+    
+    // Could be useful, maybe delete later, filtering
+    func getFavorites() -> Array<Note> {
+         return notes.filter {$0.isFavourite}
+    }
+    
+    
+    private func isNoteNameUnique(_ name: String) -> Bool {
+        return notes.filter({ $0.name == name }).count == 0
+    }
+}
